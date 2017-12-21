41510f000513708251c69eb4cb9040720c6bbe8e
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2017-12-21 12:14:02 -0500

    L1. preference scores (empty)

---
+++ PreferenceScores/EQ5D.py
@@ -0,0 +1,10 @@
+
+# EQ-5D regression coefficients
+Constant = 0.081
+N3 = 0.269
+dictCoefficients = {'Mobility':             [0, 0.069, 0.314],
+                    'Self-Care':            [0, 0.104, 0.214],
+                    'Usual Activity':       [0, 0.036, 0.094],
+                    'Pain/Discomfort':      [0, 0.123, 0.386],
+                    'Anxiety/Depression':   [0, 0.071, 0.236]};
+
